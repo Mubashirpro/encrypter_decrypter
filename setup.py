e48fec09e8c71d792db3eaba3be59991ee9fa134
Mubashir Ali <110976774+Mubashirpro@users.noreply.github.com>
2024-08-29 15:18:01 +0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,38 @@
+try:
+    from colorama import Fore
+    import base64
+    import os
+    import requests
+    mods = ["colorama","base64"]
+    if ImportError:
+       if not requests.ConnectionError:
+        for i in mods:
+            os.system(f"pip install {i}")  
+       else:
+           print("Connection failed!")
+    os.system("cls") 
+
+            
+
+except:
+    print(":(")
+red,green,cyan,lime,yellow = Fore.RED,Fore.GREEN,Fore.CYAN,Fore.LIGHTGREEN_EX,Fore.YELLOW
+#J5VQ====
+class ED:
+
+    
+    def __init__(self):
+       
+        title = """ << CODE BY MUBASHIR >> """
+        print(yellow,title)
+    def encrypter(self,string):
+        string = string.encode()
+        enc = base64.b32encode(string)
+        print(red,f"\nEncrypted : {enc.decode()}")
+    def decrypter(self,encrypted):
+        decrypted = base64.b32decode(encrypted)
+        print(lime,f"\nDecrypted : {decrypted.decode()}")
+    
+if __name__ == "__main__":
+    ed = ED()
+   
