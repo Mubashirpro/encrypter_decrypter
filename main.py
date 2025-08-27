028cf8776d172b9ab6eabf4e0d57ce34451493f2
Mubashir Ali <110976774+Mubashirpro@users.noreply.github.com>
2025-08-27 20:35:13 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
 from setup import *
+import os
 
 ed = ED()
-
+print("code by Mubashir Ali")
 while True:
     menu  = input("\nSelect option : \n[1] Encrypter \n[2] Decrypter \n[3] Exit\nSelect : ")
     if menu == "1" :
        
         user = str(input("\n[+] Enter text to encrypt : "))
+        os.system("cls")
+        print(f"text:{user}")
         ed.encrypter(user)
+        
         continue
     elif menu == "2":
         user = str(input("\n[+] Enter text to decrypt : "))
+        os.system("cls")
+        print(f"encoded:{user}")
         ed.decrypter(user)
         continue
     elif menu == "3":
